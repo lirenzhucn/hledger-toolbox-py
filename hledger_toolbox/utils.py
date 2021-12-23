5482b7bf2e84115d52e22c0b2e646402f9bb22dd
Liren Zhu <liren.zhu.cn@gmail.com>
2021-12-23 13:15:46 -0800

    add get_commodity_lots()

--- hledger_toolbox/utils.py
+++ hledger_toolbox/utils.py
@@ -1,3 +1,4 @@
+import csv
 from dataclasses import dataclass
 from datetime import datetime
 import decimal
@@ -137,3 +138,90 @@ def get_raw_text_of_pdf(input_path: str) -> str:
         )
         with open(txt_path, "r") as fp:
             return fp.read()
+
+
+@dataclass
+class CommodityLot:
+    date: datetime
+    commodity: str
+    quantity: decimal.Decimal
+    price: Price
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}({self.date.strftime('%Y-%m-%d')} "
+            f"{self.quantity:.6f} {self.commodity} {str(self.price)})"
+        )
+
+
+def get_commodity_lots(
+    base_account: str,
+    commodity_symbol: str,
+    *,
+    hledger_bin: str = "hledger",
+    hledger_file: Optional[str] = None,
+):
+    """Get the date, quantity, and price
+
+    Parameters
+    ----------
+    base_account: str
+        the base account that holds commodities
+    commodity_symbol: str
+        the symbol of the commodity to be queried
+    hledger_bin: str (optional, keyword only)
+        path to the hledger binary; default `hledger`
+    hledger_file: str | None (optional, keyword only)
+        path to the hledger journal file; default is None and will use the one
+        in $LEDGER_FILE environment variable
+
+    Returns
+    -------
+    List[CommodityLot]
+        A list of commodity lots
+    """
+    commodity_account = f"{base_account}:{commodity_symbol.lower()}"
+    cmd_commodity = [hledger_bin]
+    if hledger_file is not None:
+        cmd_commodity += ["-f", hledger_file]
+    cmd_commodity += ["bal", commodity_account, "-O", "csv"]
+    logger.info("hledger command to run: %s", " ".join(cmd_commodity))
+    process_commodity = subprocess.run(cmd_commodity, check=True, capture_output=True)
+    cmd_cost_basis = cmd_commodity + ["-B"]
+    logger.info("hledger command to run: %s", " ".join(cmd_cost_basis))
+    process_cost_basis = subprocess.run(cmd_cost_basis, check=True, capture_output=True)
+    # remove the first line (title) and the last line (total)
+    csv_reader_commodity = list(
+        csv.reader(process_commodity.stdout.decode().strip().split("\n")[1:-1])
+    )
+    csv_reader_cost_basis = list(
+        csv.reader(process_cost_basis.stdout.decode().strip().split("\n")[1:-1])
+    )
+    res = []
+    for row_commodity, row_cost_basis in zip(
+        csv_reader_commodity, csv_reader_cost_basis
+    ):
+        date = datetime.strptime(
+            row_commodity[0][len(commodity_account) + 1 :], "%Y%m%d"
+        )
+        quantity = decimal.Decimal(
+            row_commodity[1].strip()[: -len(commodity_symbol)].strip()
+        )
+        total_amount = Amount.from_dollar_string(row_cost_basis[1].strip())
+        unit_price = Price(
+            price_type=PriceType.UNIT,
+            amount=Amount(
+                commodity=total_amount.commodity,
+                formatter=total_amount.formatter,
+                value=total_amount.value / quantity,
+            ),
+        )
+        res.append(
+            CommodityLot(
+                date=date,
+                commodity=commodity_symbol,
+                quantity=quantity,
+                price=unit_price,
+            )
+        )
+    return res
