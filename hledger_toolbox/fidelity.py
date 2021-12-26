67b404c16407a9f15cbc139db9b9ec32e1670d83
Liren Zhu <liren.zhu.cn@gmail.com>
2021-12-26 06:53:29 -0800

    implement fidelity import

--- hledger_toolbox/fidelity.py
+++ hledger_toolbox/fidelity.py
@@ -1,11 +1,12 @@
 import csv
-from dataclasses import dataclass, field
-from datetime import datetime, timedelta
+import dataclasses
+from datetime import datetime
 import decimal
 import json
 import logging
 import os
 import re
+import sys
 from typing import Callable, Dict, List, Optional, Tuple
 
 import click
@@ -16,7 +17,7 @@ logger = logging.getLogger(os.path.basename(__file__))
 LOTS_MANAGER = utils.CommodityLotsManager()
 
 
-@dataclass
+@dataclasses.dataclass
 class RowParserConfig:
     base_account: str
     transfer_account: str
@@ -24,51 +25,8 @@ class RowParserConfig:
     rsu_account: str
     short_term_account: str
     long_term_account: str
-    cash_commodity: List[str] = field(default_factory=lambda: ["SPAXX"])
-
-
-def _buy_action_parser(
-    row: Dict[str, str], config: RowParserConfig
-) -> Optional[utils.Transaction]:
-    date = datetime.strptime(row["date"].strip(), "%m/%d/%Y")
-    total_dollars = decimal.Decimal(row["amount"].strip())
-    total_quantity = decimal.Decimal(row["quantity"].strip())
-    unit_price = utils.Price(
-        price_type=utils.PriceType.UNIT,
-        amount=-utils.Amount(
-            commodity="$",
-            formatter="{commodity:s}{value:.6f}",
-            value=total_dollars / total_quantity,
-        ),
-    )
-    commodity = row["symbol"].strip()
-    LOTS_MANAGER.add_lot(
-        commodity, config.base_account, date, total_quantity, unit_price
-    )
-    if commodity in config.cash_commodity:
-        # skip buy transactions on cash commodity
-        return None
-    tags = []
-    if row["action"].strip().lower().startswith("you bought espp"):
-        tags.append(("espp", ""))
-    postings = [
-        utils.Posting(
-            account=f"{config.base_account}:cash",
-            amount=utils.Amount.dollar_amount(total_dollars),
-        ),
-        utils.Posting(
-            account=f"{config.base_account}:{commodity.lower()}:{date.strftime('%Y%m%d')}",
-            amount=utils.Amount(
-                commodity=commodity,
-                formatter="{value:.6f} {commodity:s}",
-                value=total_quantity,
-            ),
-            price=unit_price,
-        ),
-    ]
-    return utils.Transaction(
-        date=date, description=row["action"].strip(), postings=postings, tags=tags
-    )
+    trade_fees_account: str
+    cash_commodity: List[str] = dataclasses.field(default_factory=lambda: ["SPAXX"])
 
 
 def _transfer_action_parser(
@@ -126,49 +84,48 @@ def _rsu_action_parser(
     )
 
 
-def _sell_action_parser(
+def _trade_action_parser(
     row: Dict[str, str], config: RowParserConfig
 ) -> Optional[utils.Transaction]:
     date = datetime.strptime(row["date"].strip(), "%m/%d/%Y")
+    commodity = row["symbol"].strip().lstrip("+-")
     total_dollars = decimal.Decimal(row["amount"].strip())
-    total_quantity = -decimal.Decimal(row["quantity"].strip())
+    total_quantity = decimal.Decimal(row["quantity"].strip())
+    change_in_quantity = total_quantity
     try:
         lot_date = datetime.strptime(row["acquired_date"].strip(), "%m/%d/%Y")
+        change_in_quantity = [(total_quantity, lot_date)]
     except ValueError:
-        raise ValueError("unable to parse lot; sell action must include lot info")
-    commodity = row["symbol"].strip()
+        pass
     if commodity in config.cash_commodity:
-        # skip buy transactions on cash commodity
+        # skip transactions on cash commodity
         return None
-    lot = LOTS_MANAGER.get_lot(commodity, config.base_account, lot_date)
-    if lot is None:
-        raise ValueError(
-            f"unable to find lot {config.base_account}:{commodity}:{lot_date.strftime('%Y%m%d')}"
-        )
-    postings = [
-        utils.Posting(
-            account=f"{config.base_account}:cash",
-            amount=utils.Amount.dollar_amount(total_dollars),
+    tags = []
+    if row["action"].strip().lower().startswith("you bought espp"):
+        tags.append(("espp", ""))
+    transaction = utils.trade_lots(
+        lots_manager=LOTS_MANAGER,
+        accounts=utils.TradeLotsAccounts(
+            base_account=config.base_account,
+            short_term_account=config.short_term_account,
+            long_term_account=config.long_term_account,
         ),
-        utils.Posting(
-            account=f"{config.base_account}:{commodity.lower()}:{lot_date.strftime('%Y%m%d')}",
-            amount=utils.Amount(
-                commodity=commodity,
-                formatter="{value:.6f} {commodity:s}",
-                value=-total_quantity,
-            ),
-            price=lot.price,
-        ),
-        utils.Posting(
-            account=config.long_term_account
-            if date - lot.date >= timedelta(days=365)
-            else config.short_term_account
-        ),
-    ]
-    LOTS_MANAGER.sell_from_lot(commodity, config.base_account, lot_date, total_quantity)
-    return utils.Transaction(
-        date=date, description=row["action"].strip(), postings=postings
+        date=date,
+        commodity=commodity,
+        change_in_quantity=change_in_quantity,
+        proceeds_or_costs=total_dollars,
     )
+    transaction.description = row["action"].strip()
+    transaction.tags = tags
+    utils.balance_transaction(transaction, config.trade_fees_account)
+    return transaction
+
+
+def _expired_option_action_parser(
+    row: Dict[str, str], config: RowParserConfig
+) -> Optional[utils.Transaction]:
+    row["amount"] = "0"
+    return _trade_action_parser(row, config)
 
 
 def _dividend_action_parser(
@@ -193,17 +150,114 @@ def _dividend_action_parser(
     )
 
 
-def _options_sell_action_parser(
-    row: Dict[str, str], config: RowParserConfig
-) -> Optional[utils.Transaction]:
-    # 04/09/2021,
-    # YOU SOLD OPENING TRANSACTION CALL (MSFT) MICROSOFT CORP MAY 14 21 $300 (100 SHS) (Cash),
-    # -MSFT210514C300, CALL (MSFT) MICROSOFT CORP MAY 14 21 $300 (100 SHS),Cash,
-    # -2,0.3,1.3,0.08,,58.62,04/12/2021,
-    date = datetime.strptime(row["date"].strip(), "%m/%d/%Y")
-    total_dollars = decimal.Decimal(row["amount"].strip())
-    commodity_account = row["symbol"].strip().strip("-+")
-    commodity = utils.map_options_commodity_symbol(commodity_account)
+class _SplitParser:
+    _inst: Optional["_SplitParser"] = None
+
+    def __init__(
+        self,
+        lots_manager: Optional[utils.CommodityLotsManager] = None,
+        base_account: str = "",
+    ) -> None:
+        self._lots_manager = lots_manager
+        self._base_account = base_account
+        self._cache: Dict[
+            str, Tuple[List[utils.CommodityLot], decimal.Decimal, decimal.Decimal]
+        ] = {}
+
+    @property
+    def lots_manager(self) -> utils.CommodityLotsManager:
+        return self._lots_manager
+
+    @lots_manager.setter
+    def lots_manager(self, lots_manager: utils.CommodityLotsManager):
+        self._lots_manager = lots_manager
+
+    @property
+    def base_account(self) -> str:
+        return self._base_account
+
+    @base_account.setter
+    def base_account(self, base_account: str):
+        self._base_account = base_account
+
+    def __call__(
+        self, row: Dict[str, str], config: RowParserConfig
+    ) -> Optional[utils.Transaction]:
+        if self._lots_manager is None or not self._base_account:
+            raise ValueError("parser not initialized")
+        date = datetime.strptime(row["date"].strip(), "%m/%d/%Y")
+        commodity = row["symbol"].strip()
+        quantity = decimal.Decimal(row["quantity"].strip())
+        if commodity in self._cache:
+            lots = self._cache[commodity][0]
+            total_quantity = sum(lot.quantity for lot in lots)
+            if quantity * total_quantity <= 0:
+                # the "sell" half of the split
+                # self._cache[commodity][1] = quantity
+                self._cache[commodity] = (lots, quantity, self._cache[commodity][2])
+            else:
+                # self._cache[commodity][2] = quantity
+                self._cache[commodity] = (lots, self._cache[commodity][1], quantity)
+            if row["action"].strip().lower().startswith("reverse"):
+                desc = f"Reverse split {commodity}"
+            else:
+                desc = f"Split {commodity}"
+            sell_quantity, buy_quantity = self._cache[commodity][1:]
+            ratio = -sell_quantity / buy_quantity
+            postings: List[utils.Posting] = []
+            for lot in lots:
+                lot_account = (
+                    f"{config.base_account}:{commodity.lower()}:"
+                    f"{lot.date.strftime('%Y%m%d')}"
+                )
+                postings.append(
+                    utils.Posting(
+                        account=lot_account,
+                        amount=utils.Amount(
+                            commodity=lot.commodity,
+                            formatter="{value:.6f} {commodity:s}",
+                            value=-lot.quantity,
+                        ),
+                        price=lot.price,
+                    )
+                )
+                new_price = dataclasses.replace(lot.price)
+                new_price.amount = dataclasses.replace(lot.price.amount)
+                new_price.amount.value *= ratio
+                postings.append(
+                    utils.Posting(
+                        account=lot_account,
+                        amount=utils.Amount(
+                            commodity=lot.commodity,
+                            formatter="{value:.6f} {commodity:s}",
+                            value=lot.quantity / ratio,
+                        ),
+                        price=new_price,
+                    )
+                )
+            transaction = utils.Transaction(
+                date=date, description=desc, postings=postings, tags=[("split", "")]
+            )
+            utils.balance_transaction(transaction, account=config.trade_fees_account)
+            return transaction
+        else:
+            lots = self._lots_manager.get_lots(commodity, self._base_account)
+            total_quantity = sum(lot.quantity for lot in lots)
+            if quantity * total_quantity <= 0:
+                # the "sell" half of the split
+                sell_quantity = quantity
+                buy_quantity = None
+            else:
+                buy_quantity = quantity
+                sell_quantity = None
+            self._cache[commodity] = (lots, sell_quantity, buy_quantity)
+            return None
+
+    @classmethod
+    def inst(cls) -> "_SplitParser":
+        if cls._inst is None:
+            cls._inst = cls()
+        return cls._inst
 
 
 ACTION_PARSER_MAP: List[
@@ -213,26 +267,28 @@ ACTION_PARSER_MAP: List[
     ]
 ] = [
     (
-        re.compile(
-            r"^\s*(reinvestment|you bought)\s+(?!closing transaction).*$", re.IGNORECASE
-        ),
-        _buy_action_parser,
+        re.compile(r"^\s*(reinvestment|you bought).*$", re.IGNORECASE),
+        _trade_action_parser,
     ),
     (
         re.compile(
-            r"^\s*(transferred|journaled spp purchase credit)\s+.*$", re.IGNORECASE
+            r"^\s*(transferred|journaled spp purchase credit|"
+            r"electronic funds transfer)\s+.*$",
+            re.IGNORECASE,
         ),
         _transfer_action_parser,
     ),
-    (
-        re.compile(r"^\s*you sold\s+(?!opening transaction).*$", re.IGNORECASE),
-        _sell_action_parser,
-    ),
+    (re.compile(r"^\s*you sold.*$", re.IGNORECASE), _trade_action_parser),
     (
         re.compile(r"^\s*conversion shares deposited .*$", re.IGNORECASE),
         _rsu_action_parser,
     ),
     (re.compile(r"^\s*dividend received .*$", re.IGNORECASE), _dividend_action_parser),
+    (
+        re.compile(r"^\s*expired (call|put) .*$", re.IGNORECASE),
+        _expired_option_action_parser,
+    ),
+    (re.compile(r"^\s*reverse split .*$", re.IGNORECASE), _SplitParser.inst()),
 ]
 
 
@@ -291,6 +347,13 @@ def _row_parser(
     default="revenues:investment:realized long term gain",
     help="hledger account name for long-term gain revenues",
 )
+@click.option(
+    "-f",
+    "--trade-fees-account",
+    type=str,
+    default="expenses:investment:trading fees",
+    help="hledger account name for trading fees",
+)
 def fidelity_import(
     input_file: str,
     output_file: str,
@@ -300,6 +363,7 @@ def fidelity_import(
     rsu_account: str,
     short_term_account: str,
     long_term_account: str,
+    trade_fees_account: str,
 ):
     """
     Import Fidelity csv statements (INPUT_FILE) into hledger friendly journal
@@ -312,7 +376,6 @@ def fidelity_import(
             for line in input_fp.readlines()
             if valid_line_regex.search(line) is not None
         ]
-    # csv_reader = csv.reader(lines)
     csv_reader = csv.DictReader(
         lines,
         [
@@ -334,6 +397,8 @@ def fidelity_import(
     rows = sorted(
         csv_reader, key=lambda r: datetime.strptime(r["date"].strip(), "%m/%d/%Y")
     )
+    _SplitParser.inst().lots_manager = LOTS_MANAGER
+    _SplitParser.inst().base_account = account
     row_parser_config = RowParserConfig(
         base_account=account,
         transfer_account=transfer_account,
@@ -341,15 +406,22 @@ def fidelity_import(
         rsu_account=rsu_account,
         short_term_account=short_term_account,
         long_term_account=long_term_account,
+        trade_fees_account=trade_fees_account,
     )
     transactions = [
         item
         for item in [_row_parser(row, row_parser_config) for row in rows]
         if item is not None
     ]
-    for transaction in transactions:
-        print(str(transaction))
-        print()
+    if output_file == "-":
+        output_fp = sys.stdout
+    else:
+        output_fp = open(output_file, "w")
+    utils.write_journal_file(
+        output_fp,
+        [("All Transactions"), transactions],
+        (min(t.date for t in transactions), max(t.date for t in transactions)),
+    )
 
 
 if __name__ == "__main__":
